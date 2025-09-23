d083217dacdc0dc71660379a4db336d92c4a7af1
Nick Fedorenko <nfm12121994@gmail.com>
2025-09-23 11:54:05 +0300

    Add files via upload

---
+++ autotests-project/UI_homework/frontend_tests_lesson/src/frontend/services/shop/shop_service_pages_manager.py
@@ -0,0 +1,12 @@
+from UI_homework.frontend_tests_lesson.src.frontend.services.shop.pages.card_page.card_page import CardPage
+from UI_homework.frontend_tests_lesson.src.frontend.services.shop.pages.catalog_page.catalog_page import CatalogPage
+from UI_homework.frontend_tests_lesson.src.frontend.services.shop.pages.login_page.login_page import LoginPage
+from UI_homework.frontend_tests_lesson.src.frontend.src.base_page.base_page import BasePage
+
+
+class ShopServicePagesManager:
+
+    def __init__(self, base_page: BasePage):
+        self.login_page = LoginPage(base_page=base_page)
+        self.catalog_page = CatalogPage(base_page)
+        self.card_page = CardPage(base_page)
